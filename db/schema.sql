8f5772334b3ddbc84e9f5488aea9ab89fe2b59e5
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-06-22 10:58:28 -0500

    4.3 create and populate a voters table

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
 
 /* TEXT data type can store much longer strings of varying length
     over use if TEXT fields can bloat the database because MySQL will allocate the maximum amount of space for a TEXT value
@@ -22,3 +23,21 @@ CREATE TABLE candidates (
  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
 
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL
+);
+
+/*capture the date and time when the voter registered*/
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  /*with DEFAULT you can specify what the value should be if no value is provided or else
+      a it could potentially be NULL if the that value isn't provided in an INSERT statement*/
+  /* CURRENT_TIMESTAMP will return the current date and time in the same 2020-01-01 13:00:00 format*/
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
